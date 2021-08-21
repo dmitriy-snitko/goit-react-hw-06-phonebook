34fd21d5b085e684ce8861a32d4337a91e96b8ac
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-08-21 22:00:37 +0300

    Update contacts-reducer.js

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,19 +1,19 @@
 import { combineReducers } from "redux";
 import { createReducer } from '@reduxjs/toolkit';
-import * as actions from '../contacts/contacts-actions';
+import {addContact, deleteContact, changeFilter} from '../contacts/contacts-actions';
 
 const items = createReducer([], {
-  [actions.addContact]: (state, { payload }) => addContact(state, payload),
+  [addContact]: (state, { payload }) => onAddContact(state, payload),
 
-  [actions.deleteContact]: (state, { payload }) =>
+  [deleteContact]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
 
 const filter = createReducer('', {
-  [actions.changeFilter]: (_, { payload }) => payload,
+  [changeFilter]: (_, { payload }) => payload,
 });
 
-const addContact = (state, payload) => {
+const onAddContact = (state, payload) => {
   const normalizedName = payload.name.toLowerCase();
   const isAlreadyinContacts = state.find(el => el.name.toLowerCase() === normalizedName);
 
